ed2c64839fb3c00d7d6b12e3a2e432e18bdfc09e
maya <maya04@keemail.me>
2024-12-17 19:52:35 -0800

    INITIAL LAUNCH: LIVE

--- build/actions.js
+++ build/actions.js
@@ -5,3 +5,10 @@
 // /    |    \     \____|    |   |   /    |    \/    |    \/        \
 // \____|__  /\______  /|____|   |___\_______  /\____|__  /_______  /
 //       \/        \/                      \/         \/        \/ 
+
+// -------------- VARIABLES :::
+
+var photos = document.getElementById('photos');
+var videos = document.getElementById('videos');
+var year24 = document.getElementById('year24');
+var year25 = document.getElementById('year25');
